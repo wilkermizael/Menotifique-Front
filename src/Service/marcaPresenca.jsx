ad3c996fb31c36aa565c2abee56097e7dc132ff3
Wilker Mizael <wilkermizael@Wilker-Macbook-Pro.local>
2024-12-31 02:08:03 -0300

    refeact/solucionando duplicidade na presença

--- src/Service/marcaPresenca.jsx
+++ src/Service/marcaPresenca.jsx
@@ -3,17 +3,18 @@ import { comparaData } from "../Utils/comparaData";
 import { evolution } from "./evolution";
 
 const marcaPresenca = async (lista) => {
-  /*const dataDb = await verificaChamada(lista);
+  const dataDb = await verificaChamada(lista);
   console.log(dataDb)
   const chamadaJaRealizada = await comparaData(dataDb);
-  return chamadaJaRealizada*/
+  if (chamadaJaRealizada == "turma_ja_chamada") {
+    return "turma_ja_chamada";
+  }
+
   try {
     // Verifica se a chamada já foi realizada
-    const dataDb = await verificaChamada(lista);
-    const chamadaJaRealizada = await comparaData(dataDb);
-    if (chamadaJaRealizada == "turma_ja_chamada") {
-      return "turma_ja_chamada";
-    }
+    /*const dataDb = await verificaChamada(lista);
+    const chamadaJaRealizada = await comparaData(dataDb);*/
+    
 
     // Função para formatar a data como YYYY-MM-DD
     const formatDate = (date) => {
