261ccc496754b287361f3da892dbafc27c1df789
Wilker Mizael <wilkermizael@Wilker-Macbook-Pro.local>
2025-01-01 20:35:45 -0300

    ajuste paginacao

--- src/Components/BottomNavigation.jsx
+++ src/Components/BottomNavigation.jsx
@@ -1,84 +1,89 @@
-import { Box, Tab, Tabs} from "@mui/material";
+import { Box, Tab, Tabs } from "@mui/material";
 import PropTypes from "prop-types";
 import Chamada from "./Chamada";
 import NewTabela from "./newTable";
-import BuscaAtiva from "../Pages/Turma/BuscaAtiva"
+import BuscaAtiva from "../Pages/Turma/BuscaAtiva";
 import DiarioDeBordo from "../Pages/Turma/DiarioDeBordo";
-//import React from "react";
+
 function CustomTabPanel(props) {
-    const { children, value, index, ...other } = props;
-  
-    return (
-      <div
-        role="tabpanel"
-        hidden={value !== index}
-        id={`simple-tabpanel-${index}`}
-        aria-labelledby={`simple-tab-${index}`}
-        {...other}
-      >
-        {value === index && <Box sx={{ p: 3 }}>{children}</Box>}
-      </div>
-    );
-  }
-  
-  CustomTabPanel.propTypes = {
-    children: PropTypes.node,
-    index: PropTypes.number.isRequired,
-    value: PropTypes.number.isRequired,
-  };
-  
-  function a11yProps(index) {
-    return {
-      id: `simple-tab-${index}`,
-      'aria-controls': `simple-tabpanel-${index}`,
-    };
-  }
-const MyBottomNavigation = ({value, setValue, turmaId})=>{
+  const { children, value, index, ...other } = props;
 
-    const handleChange = (event, newValue) => {
-      setValue(newValue);
-    };
-  
-    return (
-      <Box sx={{ 
-        width: '100%', 
-
-      }}>
-        <Box sx={{ 
-          borderBottom: 1, 
-          borderColor: 'divider', 
-          display:"flex",
-          justifyContent:"center",
-          height: '100vh', 
-          overflow: 'hidden'
-          }}>
-          <Tabs value={value} onChange={handleChange} aria-label="basic tabs example">
-            <Tab label="Chamada" {...a11yProps(0)} />
-            <Tab label="Alunos" {...a11yProps(1)} />
-            <Tab label="Busca Ativa" {...a11yProps(2)}/>
-            <Tab label="Diário de Bordo" {...a11yProps(3)}/>
-          </Tabs>
-        </Box>
-        <CustomTabPanel value={value} index={0}>
-          <Chamada turmaId={turmaId}/>
-        </CustomTabPanel>
-        <CustomTabPanel value={value} index={1}>
-          <NewTabela turmaId={turmaId}/>
-        </CustomTabPanel>
-        <CustomTabPanel value={value} index={2}>
-          <BuscaAtiva turmaId={turmaId}/>
-        </CustomTabPanel>
-        <CustomTabPanel value={value} index={3}>
-          <DiarioDeBordo turmaId={turmaId}/>
-        </CustomTabPanel>
-      </Box>
-    );
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && <Box sx={{ p: 2 }}>{children}</Box>}
+    </div>
+  );
 }
 
-MyBottomNavigation.propTypes = {
-    value: PropTypes.number.isRequired,
-    setValue: PropTypes.func.isRequired,
-    turmaId: PropTypes.number.isRequired,
-   
+CustomTabPanel.propTypes = {
+  children: PropTypes.node,
+  index: PropTypes.number.isRequired,
+  value: PropTypes.number.isRequired,
+};
+
+function a11yProps(index) {
+  return {
+    id: `simple-tab-${index}`,
+    'aria-controls': `simple-tabpanel-${index}`,
   };
-export default MyBottomNavigation
+}
+
+const MyBottomNavigation = ({ value, setValue, turmaId }) => {
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  return (
+    <Box sx={{ width: "100%" }}>
+      <Box
+        sx={{
+          borderBottom: 1,
+          borderColor: "divider",
+          display: "flex",
+          justifyContent: "center",
+          width: "100%",
+          overflow: "hidden", // Remove qualquer rolagem
+          flexWrap: "wrap", // Permite que os elementos se ajustem
+        }}
+      >
+        <Tabs
+          value={value}
+          onChange={handleChange}
+          aria-label="basic tabs example"
+          variant="fullWidth" // Garante que as abas se ajustem automaticamente
+        >
+          <Tab label="Chamada" {...a11yProps(0)} />
+          <Tab label="Alunos" {...a11yProps(1)} />
+          <Tab label="Busca Ativa" {...a11yProps(2)} />
+          <Tab label="Diário de Bordo" {...a11yProps(3)} />
+        </Tabs>
+      </Box>
+      <CustomTabPanel value={value} index={0}>
+        <Chamada turmaId={turmaId} />
+      </CustomTabPanel>
+      <CustomTabPanel value={value} index={1}>
+        <NewTabela turmaId={turmaId} />
+      </CustomTabPanel>
+      <CustomTabPanel value={value} index={2}>
+        <BuscaAtiva turmaId={turmaId} />
+      </CustomTabPanel>
+      <CustomTabPanel value={value} index={3}>
+        <DiarioDeBordo turmaId={turmaId} />
+      </CustomTabPanel>
+    </Box>
+  );
+};
+
+MyBottomNavigation.propTypes = {
+  value: PropTypes.number.isRequired,
+  setValue: PropTypes.func.isRequired,
+  turmaId: PropTypes.number.isRequired,
+};
+
+export default MyBottomNavigation;
